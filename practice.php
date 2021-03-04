9c71bd3436a2ba92eb9122326b515e2dd4943fa1
吉富　章博 <jifuzhangbo@gmail.com>
2021-03-04 08:42:08 +0000

    課題No11

---
+++ practice.php
@@ -0,0 +1,27 @@
+//課題１
+<?php 
+$a=3;
+$b=7;
+echo $a+$b;?>
+
+//課題２
+<?php
+$array_month=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7]; ?>
+
+// 課題３
+<?php
+$hello="Hello";
+$name="吉富 章博";
+$world="‘s World!";
+$hello.=$name;
+$hello.=$world;
+echo $hello;
+?>
+
+// 課題4
+<?php
+$tech_boost="tech";
+$tech_boost.="_boost";
+echo $tech_boost;
+?>
